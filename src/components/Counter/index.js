c53fe4215b0425e7db3993cfbb1bbeaab36f3382
PavanGangireddy <pavangangireddy@ibhubs.co>
2021-03-05 15:35:12 +0530

    checking lint staged

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -9,7 +9,7 @@ class Counter extends Component {
     return (
       <div>
         <div onClick={this.onDecrement}>-</div>
-        <div style={styless.counter}>{this.state.counter}</div>
+        <div style={stylesssss.counter}>{this.state.counter}</div>
         <div style={buttons} onClick={this.onIncrement}>
           +
         </div>

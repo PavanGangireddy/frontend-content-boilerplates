daf6fd19cc01bd26f8a8778e80e7c426ce6a8d8a
PavanGangireddy <pavangangireddy@ibhubs.co>
2021-03-05 19:05:22 +0530

    checking console

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,3 @@
 /* eslint-disable */
 
-import 'https://gist.github.com/PavanGangireddy/fa3ea3329787c105e12414160b0f3d47'
 import 'https://gist.github.com/PavanGangireddy/a0a6c8084264081d4b03b5ec6f0b72da'

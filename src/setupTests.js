b07caf109ddfce80b220c09488d9aa53d3fe6a5c
PavanGangireddy <pavangangireddy@ibhubs.co>
2021-03-19 09:15:38 +0530

    session bolierplate setup changed

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,6 @@
 /* eslint-disable */
 
-import 'https://gist.github.com/PavanGangireddy/a0a6c8084264081d4b03b5ec6f0b72da'
+import '@testing-library/jest-dom'
+import {configure} from '@testing-library/react'
+
+configure({testIdAttribute: 'testid'})

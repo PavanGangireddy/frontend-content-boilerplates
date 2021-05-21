ba7869ad9c9e6eb37d7c3f550c8cd65f70040173
Sri Lalitha Rani Kintali <lalitha.kintali@ibhubs.co.in>
2021-05-21 16:18:05 +0530

    updated cp boilerplate

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,23 +1,11 @@
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
-import App from "../App";
+import App from '../App'
 
-describe("App tests", () => {
-  beforeAll(() => {
-    jest.spyOn(console, "error");
-  });
-
-  afterAll(() => {
-    console.error.mockRestore();
-  });
-
-  afterEach(() => {
-    expect(console.error).not.toHaveBeenCalled();
-  });
-
-  it("display text:::5:::", () => {
-    render(<App />);
-    expect(true).toBe(true);
-  });
-});
+describe('App tests', () => {
+  it('display text:::5:::', () => {
+    render(<App />)
+    expect(true).toBe(true)
+  })
+})
